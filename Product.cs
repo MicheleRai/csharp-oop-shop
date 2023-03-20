ce32cf64eca64b22da1e521ddd5b40cdb2ee206d
MicheleRai <rainonemichele1999@gmail.com>
2023-03-20 16:16:10 +0100

    completo con iva

---
+++ Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Prodotto
+{
+    public int Codice { get; set; }
+    public string? NomeProdotto { get; set; }
+    public string? DescrizioneProdotto { get; set; }
+    public double? PrezzoProdotto { get; set; }
+    public int Iva { get; set; }
+
+    public Prodotto(string nome, string descrizione, double prezzo, int iva)
+    {
+        Codice = new Random().Next(1, 99999999);
+        NomeProdotto = nome;
+        DescrizioneProdotto = descrizione;
+        PrezzoProdotto = prezzo;
+        Iva = iva;
+    }
+    public void Print()
+    {
+        double prezzoConIva(double PrezzoProdotto, int Iva)
+        {
+            double IvaPrezzo = PrezzoProdotto * Iva;
+
+            double prezzoConIva = PrezzoProdotto + IvaPrezzo;
+
+
+            return prezzoConIva;
+        }
+
+        Console.WriteLine(NomeProdotto);
+        Console.Write("Descrizione: ");
+        Console.WriteLine(DescrizioneProdotto);
+        Console.Write("Prezzo compreso di iva: ");
+        Console.WriteLine($"{prezzoConIva}");
+        Console.Write("IVA: ");
+        Console.WriteLine(Iva);
+
+    }
+   
+
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.WriteLine("Inserisci nome prodotto");
+string nome = Console.ReadLine() ?? "";
+Console.WriteLine("Inserisci descrizione prodotto");
+string descrizione = Console.ReadLine() ?? "";
+Console.WriteLine("Inserisci prezzo prodotto");
+double prezzo = double.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Inserisci l'iva relativa al prodotto");
+int iva = int.Parse(Console.ReadLine() ?? "");
+
+
+
+
+Prodotto prodotto = new(nome, descrizione, prezzo, iva);
+
+prodotto.Print();
