e08a7d7d5c2f56d15bfe6b607d7921025e20fc1c
MicheleRai <rainonemichele1999@gmail.com>
2023-03-20 17:50:09 +0100

    stampa improved

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿
 
 Console.WriteLine("Inserisci nome prodotto");
-string? nome = Console.ReadLine() ?? "";
+string nome = Console.ReadLine() ?? "";
 Console.WriteLine("Inserisci descrizione prodotto");
 string descrizione = Console.ReadLine() ?? "";
 Console.WriteLine("Inserisci prezzo prodotto");
@@ -14,12 +14,10 @@ int iva = int.Parse(Console.ReadLine() ?? "");
 
 Prodotto prodotto = new(nome, descrizione, prezzo, iva);
 
+    Console.WriteLine("Il prodotto da te aggiunto èç ");
+
     Console.WriteLine(prodotto.NomeProdotto);
-    Console.Write("Codice prodotto: ");
-    Console.WriteLine(prodotto.CodiceConPad());
-    Console.Write("Descrizione: ");
-    Console.WriteLine(prodotto.DescrizioneProdotto);
-    Console.Write("Prezzo con iva: ");
-    Console.WriteLine(prodotto.PrezzoConIva());
-    Console.Write("IVA: ");
-    Console.WriteLine(iva);
+    Console.WriteLine($"Codice prodotto: { prodotto.CodiceConPad()}");
+    Console.WriteLine($"Descrizione: {prodotto.DescrizioneProdotto}");
+    Console.WriteLine($"Prezzo con iva: {prodotto.PrezzoConIva()}$");
+    Console.WriteLine($"IVA: {iva}%");
